f6e1a038c9311770f70e055e149a2c8c991630bc
raiyan <67802404+raiyanshadow@users.noreply.github.com>
2026-04-18 07:44:25 -0400

    Delete lab01 directory

--- lab01/compile_all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-gcc -Wall -g -o hello hello.c
-gcc -Wall -g -o count count.c
-gcc -Wall -g -o echo_arg echo_arg.c
-gcc -Wall -g -o echo_stdin echo_stdin.c
--- lab01/my_commands.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-./echo_arg csc209 > echo_out.txt
-./echo_stdin < echo_stdin.c
-./count 210 | wc -c
-ls -S | ./echo_stdin
